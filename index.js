fcab7cf639fd947baaad12a43638a8dbeb42a0e7
Sam Belliel <sbelliel@gmail.com>
2018-07-17 14:35:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Add your functions and code here
-kittens = ['Milo', 'Otis', 'Garfield'];
+var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
   kittens = kittens.push(name);
